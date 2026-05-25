44f7db1de76bf1215159356d7a50b9b5c0fc315f
Hanae <hanae.tekfaoui23@ump.ac.ma>
2026-05-25 15:33:42 +0100

    Ajout build React

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const path = require("path");
 
 require("dotenv").config();
-const User = require("./models/User");
 
 const app = express();
 
@@ -16,62 +15,17 @@ mongoose.connect(process.env.MONGO_URI)
 .catch((err) => console.log(err));
 
 app.get("/api", (req, res) => {
-    res.json({ message: "API fonctionne !" });
+  res.json({ message: "API fonctionne !" });
 });
 
-app.use(express.static(
-    path.join(__dirname, "../frontend/build")
-));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get(/.*/, (req, res) => {
-    res.sendFile(
-        path.join(__dirname, "../frontend/build/index.html")
-    );
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.post("/register", async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-
-    const user = new User({
-      username,
-      email,
-      password,
-    });
-
-    await user.save();
-
-    res.json({ message: "Inscription réussie" });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: "Erreur serveur" });
-  }
-});
-
-app.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ email, password });
-
-    if (!user) {
-      return res.status(400).json({
-        message: "Utilisateur introuvable",
-      });
-    }
-
-    res.json({
-      message: "Connexion réussie",
-      user,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: "Erreur serveur" });
-  }
-});
-
-app.listen(PORT, () => {
-    console.log(`Serveur lancé sur le port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Serveur lancé sur le port ${PORT}`);
 });
